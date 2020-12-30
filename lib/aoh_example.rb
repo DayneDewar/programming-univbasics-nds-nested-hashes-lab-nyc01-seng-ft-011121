d36f65f1116355dce219212cc251c77a5804bd50
Dayne Dewar <daynedewar@yahoo.com>
2020-12-30 04:25:43 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,8 @@ end
 def literal_aoh 
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  array = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  array = Array.new 
+  array << DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G
   
 end
 
@@ -26,4 +27,5 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
+  
 end
